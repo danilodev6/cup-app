3924ed8d4a56f24b090c5448cb09afe3fb78f8dd
danilo <danilozabalet@Danilos-MacBook-Air.local>
2025-12-01 00:44:58 -0300

    improve team details page

--- src/app/teams/[id]/page.tsx
+++ src/app/teams/[id]/page.tsx
@@ -22,13 +22,21 @@ export default async function TeamPage({
 
   return (
     <div className="flex flex-col items-center">
-      <img className="w-32 h-32 mt-4" src={team.logoUrl} alt={team.name} />
+      <img className="w-24 h-24 mt-4" src={team.logoUrl} alt={team.name} />
       <h2 className="text-3xl mt-4">{team.name}</h2>
-      <ul className="mt-6 grid grid-cols-3 gap-4">
+      <ul className="mt-6 flex gap-8 flex-wrap justify-center">
         {team.players.map((player) => (
           <li className="flex flex-col gap-4 items-center" key={player.id}>
             <img id="playerPhoto" src={player.photoUrl} alt={player.name} />
             <p>{player.name}</p>
+            <div className="text-sm flex gap-1 items-center">
+              <img className="w-10 h-10" src="@/../icons/goals.png" />
+              <span>{player.score}</span>
+              <img className="w-10 h-10" src="@/../icons/yellow-card.png" />
+              <span>{player.yellowCards}</span>
+              <img className="w-10 h-10" src="@/../icons/red-card.png" />
+              <span>{player.redCards}</span>
+            </div>
           </li>
         ))}
       </ul>
