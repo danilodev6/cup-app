1c1feb1aa8a30158e0bac95f23da69fbff069e20
danilo <danilozabalet@Danilos-MacBook-Air.local>
2025-12-24 10:51:26 -0300

    change about text

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,12 +4,12 @@ const About = () => {
       {" "}
       <h1>About</h1>;
       <p>
-        La app de torneos de Don Balón es una plataforma diseñada para la
-        organización y gestión de torneos de fútbol 5. La aplicación permite
-        administrar las competencias de forma simple y ordenada, desde la
-        creación de torneos y equipos hasta el seguimiento de partidos y
-        resultados en tiempo real. Su objetivo es facilitar el trabajo de los
-        organizadores y mejorar la experiencia de los jugadores.
+        La app de torneos es una plataforma diseñada para la organización y
+        gestión de torneos de fútbol 5. La aplicación permite administrar las
+        competencias de forma simple y ordenada, desde la creación de torneos y
+        equipos hasta el seguimiento de partidos y resultados en tiempo real. Su
+        objetivo es facilitar el trabajo de los organizadores y mejorar la
+        experiencia de los jugadores.
       </p>
     </>
   );
